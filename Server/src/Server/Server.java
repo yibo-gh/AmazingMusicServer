2929b3a31eabbef0dc015d0bf8a3a1109ceaf299
In Young Cho <ciy405x@kaist.ac.kr>
2018-07-09 00:29:51 -0700

    Socket client testers and comments

--- Server/src/Server/Server.java
+++ Server/src/Server/Server.java
@@ -5,16 +5,24 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+	
 	public Server() {
+		/*
+		 * Constructor for Server class.
+		 * Serversocket stays to listen a client's request.
+		 * If server success to listen and accept the request from a client, 
+		 * Create a clientsocket which is connected with a client and
+		 * Create another server thread, which do all server work for the client.
+		 */
 		ServerSocket serverSocket = null;
 		Socket clientSocket;
 		
 		try {
 			serverSocket = new ServerSocket(18701);
-			
+			// Server, Keep working!
 			while (true) {
 				clientSocket = serverSocket.accept();
-				
+				// Success to listen and accept. Create another server thread.
 				new CreateServerThread(clientSocket);
 			}
 		} catch (Exception e) {
