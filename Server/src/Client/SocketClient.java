ba50a958d294217c719b4e89823747b57e873dc0
LeeDongYeun <ledoye@kaist.ac.kr>
2018-07-11 15:58:02 -0700

    fileserver add

--- Server/src/Client/SocketClient.java
+++ Server/src/Client/SocketClient.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+
 import Object.LinkedList;
 
 public class SocketClient {
