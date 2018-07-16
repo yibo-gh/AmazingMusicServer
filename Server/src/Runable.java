d88a5a67c5f8610b728bb961e8025ab13afec2f1
In Young Cho <ciy405x@kaist.ac.kr>
2018-07-16 16:58:04 -0700

    [Sprint2-TaskCoreFunctions]:SQL driver

--- Server/src/Runable.java
+++ Server/src/Runable.java
@@ -4,4 +4,5 @@ public class Runable {
 	public static void main (String args[]) {
 		Server.Runserver.main(null);
 	}
+	
 }
--- Server/src/SysTest.java
+++ Server/src/SysTest.java
@@ -4,4 +4,5 @@ public class SysTest {
 	public static void main (String args[]) {
 		System.out.println(Request.Request.login("ciy405x@kaist.ac.kr", "cofls8680*"));
 	}
+	
 }
